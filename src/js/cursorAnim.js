93b3caa5f3a280fb7c00f18e83eef139f13d6c2e
LaGordita <caden.perry.chan@gmail.com>
2024-05-12 21:55:17 -0400

    Cursor loading screen coming along

--- src/js/cursorAnim.js
+++ src/js/cursorAnim.js
@@ -2,34 +2,106 @@ let array = [];
 const windowWidth = window.innerWidth;
 const windowHeight = window.innerHeight;
 
-let gridLength = 20;
-let gridHeight = 20;
+const centerScreenX = windowWidth / 2;
+const centerScreenY = windowHeight / 2;
 
-let iconWidth = windowWidth / gridLength;
-let iconHeight = windowHeight / gridHeight;
+let iconWidth = 30;
+let iconHeight;
+let gridLength;
+let gridHeight;
 
 function gridCreation() {
-  document.getElementById('cursorGrid').style.gridTemplateColumns = "repeat(" + gridLength + ", 1fr)";
+  log("A")
+  
+  const testElement = document.createElement('div');
+  testElement.setAttribute('id', 'testElement');
 
-  for (let y = 0; y < gridHeight; y++) {
-    for (let x = 0; x < gridLength; x++) {
-      createCursor(y * gridLength + x);
+  const img = document.createElement("img");
+  img.setAttribute("src", "/Icons/cursor_outline.svg");
+  img.setAttribute("alt", "cursor outline");
+  img.setAttribute('id', 'cursorReference');
+  img.style.width = iconWidth + "px";
+
+  testElement.appendChild(img);
+  document.body.appendChild(testElement);
+
+  // Wait for the image to load before measuring its height
+  img.onload = function() {
+    log("B")
+
+    const svgElement = document.getElementById('cursorReference');
+    const svgHeight = svgElement.getBoundingClientRect().height;
+
+    iconHeight = svgHeight;
+
+    gridLength = Math.ceil(windowWidth / iconWidth);
+    gridHeight = Math.ceil(windowHeight / iconHeight);
+
+    testElement.remove();
+
+    log("C")
+
+    document.getElementById('cursorGrid').style.gridTemplateColumns = "repeat(" + gridLength + ", 1fr)";
+
+    for (let y = 0; y < gridHeight; y++) {
+      for (let x = 0; x < gridLength; x++) {
+        createCursor(y * gridLength + x);
+      }
     }
-  }
+
+    let circleCheckResolution = 1;
+    let circleCheckResolutionR = circleCheckResolution * Math.PI / 180;
+    
+    let radius = 200;
+
+    for (let i = 0; i < 360 / circleCheckResolution; i++) {
+      let checkPointX = centerScreenX + radius * Math.cos(circleCheckResolutionR * i);
+      let checkPointY = centerScreenY + radius * Math.sin(circleCheckResolutionR * i);
+
+      for (let y = 0; y < gridHeight; y++) {        
+        for (let x = 0; x < gridLength; x++) {
+          
+          if(checkPointY >= y * iconHeight && checkPointY <= y * (iconHeight + 1)) {
+            if(checkPointX >= x * iconWidth && checkPointX <= x * (iconWidth + 1)) {
+              log("Coord #" + i + ": " + checkPointX + ", " + checkPointY);
+              
+              let elementId = 'cursorOutline' + (y * gridLength + x);
+              log(elementId)
+              let img = document.getElementById(elementId)
+              img.setAttribute("src", "/Icons/cursor_solid.svg");
+            }    
+          }
+        }
+      }
+    }
+
+  };
 }
 
 export { gridCreation };
 
+window.onload = function()
+  {
+    log("D")
+
+    document.body.style.overflow = "hidden";
+  }
+
 function createCursor(num) { 
+  log("E")
+  
   const loader = document.getElementById("cursorGrid");
   const img = document.createElement("IMG");
   
   img.setAttribute("src", "/Icons/cursor_outline.svg");
   img.setAttribute("alt", "cursor outline");
-  img.classList.add("cursorOutline" + num);
+  img.setAttribute("id", "cursorOutline" + num);
   img.style.width = iconWidth + "px";
-  img.style.height = iconHeight + "px";
 
   loader.appendChild(img);
   document.body.appendChild(loader);
+}
+
+function log(message) {
+  console.log(message);
 }
