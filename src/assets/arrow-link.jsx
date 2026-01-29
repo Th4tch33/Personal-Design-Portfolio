f8f276f9761247240a8b6605a2888b11b1137896
Da Cheekon <caden.perry.chan@gmail.com>
2026-01-28 21:56:40 -0500

    updating footer design and use for a more professional look

---
+++ src/assets/arrow-link.jsx
@@ -0,0 +1,24 @@
+function IconClose() {
+  return (
+    <svg
+      width={20}
+      height={20}
+      viewBox="0 0 20 20"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M1.06055 1.06055L18.0605 18.0605"
+        stroke="currentColor"
+        strokeWidth={3}
+      />
+      <path
+        d="M0.0605469 18.0605H18.0605V0.0605469"
+        stroke="currentColor"
+        strokeWidth={3}
+      />
+    </svg>
+  );
+}
+
+export default IconClose;
